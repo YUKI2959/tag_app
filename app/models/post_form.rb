4ba52ba45e4510dd5fa5c51b60fdd42bfb403ad8
内田有貴 <117145436+YUKI2959@users.noreply.github.com>
2023-01-25 10:22:46 +0900

    Formオブジェクトを用いた編集機能を実装しよう

--- app/models/post_form.rb
+++ app/models/post_form.rb
@@ -1,3 +1,24 @@
 class PostForm
   include ActiveModel::Model
+
+  #PostFormクラスのオブジェクトがPostモデルの属性を扱えるようにする
+  attr_accessor :text, :image
+
+    with_options presence: true do
+    validates :text
+    validates :image
+  end
+
+  def save
+    Post.create(text: text, image: image)
+  end
+
+  def update(params, post)
+    post.update(params)
+  end
+
+  attr_accessor(
+    :text, :image,
+    :id, :created_at, :updated_at
+   )
 end
